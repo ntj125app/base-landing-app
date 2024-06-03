e4d308e42878095fd93563d819d439f7cbc6acd2
Nathanael Yusuf Tj <26967808+nathanaelytj@users.noreply.github.com>
2024-06-03 09:52:44 +0000

    Use trusted proxies

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -12,7 +12,7 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/app/healthcheck',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        //
+        $middleware->trustProxies(headers: Request::HEADER_HTTP_X_REAL_IP);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         if (app()->bound('sentry')) {
