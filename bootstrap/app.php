9a41bfe3766a61c55baa8c1e1c78526c4807f906
Nathanael Yusuf Tj <26967808+nathanaelytj@users.noreply.github.com>
2025-01-02 03:22:00 +0000

    Add trust proxies

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -11,7 +11,21 @@ return Application::configure(basePath: dirname(__DIR__))
         commands: __DIR__.'/../routes/console.php',
         health: '/app/healthcheck',
     )
-    ->withMiddleware(function (Middleware $middleware) {})
+    ->withMiddleware(function (Middleware $middleware) {
+        $middleware->trustProxies(at: [
+            '127.0.0.0/8',
+            '10.0.0.0/8',
+            '172.16.0.0/12',
+            '192.168.0.0/16',
+        ]);
+
+        $middleware->trustProxies(headers: Request::HEADER_X_FORWARDED_FOR |
+            Request::HEADER_X_FORWARDED_HOST |
+            Request::HEADER_X_FORWARDED_PORT |
+            Request::HEADER_X_FORWARDED_PROTO |
+            Request::HEADER_X_FORWARDED_AWS_ELB
+        );
+    })
     ->withExceptions(function (Exceptions $exceptions) {
         if (app()->bound('sentry')) {
             Integration::handles($exceptions);
